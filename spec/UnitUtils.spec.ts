21a9ce5de30181f8848e27ee61e0ce7d32fcd266
TylerBreau <32340239+TylerBreau@users.noreply.github.com>
2019-11-22 11:10:03 -0400

    Added getUnitText (#3)

--- spec/UnitUtils.spec.ts
+++ spec/UnitUtils.spec.ts
@@ -128,4 +128,62 @@ describe('UnitUtils', () => {
             expect(UnitUtils.getUnitType(Unit.INCH_OVER_MILE)).toBe(UnitType.LENGTH_OVER_LENGTH);
         });
     });
+
+    describe('Unit -> getUnitText', () => {
+        const SUPERSCRIPT_TWO: string = '\u00B2';
+        it('Unit.MILLIMETER', () => {
+            expect(UnitUtils.getUnitText(Unit.MILLIMETER)).toBe('mm');
+        });
+        it('Unit.CENTIMETER', () => {
+            expect(UnitUtils.getUnitText(Unit.CENTIMETER)).toBe('cm');
+        });
+        it('Unit.METER', () => {
+            expect(UnitUtils.getUnitText(Unit.METER)).toBe('m');
+        });
+        it('Unit.KILOMETER', () => {
+            expect(UnitUtils.getUnitText(Unit.KILOMETER)).toBe('km');
+        });
+        it('Unit.MILLIMETER_SQUARED', () => {
+            expect(UnitUtils.getUnitText(Unit.MILLIMETER_SQUARED)).toBe('mm' + SUPERSCRIPT_TWO);
+        });
+        it('Unit.CENTIMETER_SQUARED', () => {
+            expect(UnitUtils.getUnitText(Unit.CENTIMETER_SQUARED)).toBe('cm' + SUPERSCRIPT_TWO);
+        });
+        it('Unit.METER_SQUARED', () => {
+            expect(UnitUtils.getUnitText(Unit.METER_SQUARED)).toBe('m' + SUPERSCRIPT_TWO);
+        });
+        it('Unit.KILOMETER_SQUARED', () => {
+            expect(UnitUtils.getUnitText(Unit.KILOMETER_SQUARED)).toBe('km' + SUPERSCRIPT_TWO);
+        });
+        it('Unit.INCH', () => {
+            expect(UnitUtils.getUnitText(Unit.INCH)).toBe('in');
+        });
+        it('Unit.FOOT', () => {
+            expect(UnitUtils.getUnitText(Unit.FOOT)).toBe('ft');
+        });
+        it('Unit.YARD', () => {
+            expect(UnitUtils.getUnitText(Unit.YARD)).toBe('yd');
+        });
+        it('Unit.MILE', () => {
+            expect(UnitUtils.getUnitText(Unit.MILE)).toBe('mi');
+        });
+        it('Unit.INCH_SQUARED', () => {
+            expect(UnitUtils.getUnitText(Unit.INCH_SQUARED)).toBe('in' + SUPERSCRIPT_TWO);
+        });
+        it('Unit.FOOT_SQUARED', () => {
+            expect(UnitUtils.getUnitText(Unit.FOOT_SQUARED)).toBe('ft' + SUPERSCRIPT_TWO);
+        });
+        it('Unit.YARD_SQUARED', () => {
+            expect(UnitUtils.getUnitText(Unit.YARD_SQUARED)).toBe('yd' + SUPERSCRIPT_TWO);
+        });
+        it('Unit.MILE_SQUARED', () => {
+            expect(UnitUtils.getUnitText(Unit.MILE_SQUARED)).toBe('mi' + SUPERSCRIPT_TWO);
+        });
+        it('Unit.METER_OVER_KILOMETER', () => {
+            expect(UnitUtils.getUnitText(Unit.METER_OVER_KILOMETER)).toBe('');
+        });
+        it('Unit.INCH_OVER_MILE', () => {
+            expect(UnitUtils.getUnitText(Unit.INCH_OVER_MILE)).toBe('');
+        });
+    });
 });
